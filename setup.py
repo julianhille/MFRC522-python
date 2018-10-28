3d49a523887a36fb3896094943b22bf973da4c82
chme <christian.meffert@googlemail.com>
2018-10-28 07:13:04 +0100

    Add licence information
    https://github.com/mxgxw/MFRC522-python is licenced under the LGPL-3.0

--- setup.py
+++ setup.py
@@ -6,4 +6,9 @@ setup(name='MFRC522',
     version='0.0.1',
     description='Raspberry Pi Python library for SPI RFID RC522 module.',
     install_requires=['spidev', 'RPi.GPIO'],
-    packages=['MFRC522'])
+    packages=['MFRC522'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)",
+        "Operating System :: POSIX :: Linux",
+    ])
